6006194d4120c4021d6be81adeed6d2a13be8819
Igor Menezes Portela Delgado <igor.delgado@jdrel.com.br>
2022-06-27 11:37:28 -0300

    feature/adding-climem

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,21 @@ const cluster = require("cluster");
 const PORT = 3331;
 
 if (cluster.isPrimary) {
-   console.log('Master process is running');
-   cluster.fork();
- } else {
-   app.listen(PORT, () => {
-      console.log(`App running in http://localhost:${PORT}`)
-   })
- }
+  console.log("Master process is running");
+  cluster.fork();
+} else {
+  app.listen(PORT, () => {
+    console.log(`App running in http://localhost:${PORT}`);
+  });
+}
 
- cluster.on('exit', (worker: Worker, code: any, signal: any) => {
-   console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
-   console.log('Starting a new worker');
-   cluster.fork();
- });
+cluster.on("exit", (worker: Worker, code: any, signal: any) => {
+  console.log(
+    `Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`
+  );
+  console.log("Starting a new worker");
+  cluster.fork();
+});
 
 // app.listen(PORT, () => {
 //   console.log(`App running in http://localhost:${PORT}`);
